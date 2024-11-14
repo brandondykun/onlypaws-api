266fbbc904b10302c241b738792c7580d6f94bf9
Brandon Dykun <brandon.dykun@gmail.com>
2024-11-13 20:37:33 -0500

    feat: add fixture data and management commands to create and load fixture data
    Added initial dev fixture data and management commands to update and load fixture data. clear_and_load_db command only works in test env.

---
+++ api/apps/core_app/management/commands/clear_and_load_db.py
@@ -0,0 +1,34 @@
+import os
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = "Clears the database and loads data from fixtures."
+
+    def handle(self, *args, **options):
+        # Clear the database
+        if os.environ.get("DJANGO_ENV") != "test":
+            self.stdout.write(
+                self.style.ERROR("This command can only be run in a test environment!")
+            )
+            return
+        call_command("flush", "--noinput")
+
+        self.stdout.write(self.style.SUCCESS("Database cleared successfully!"))
+
+        # Load fixtures
+        fixture_paths = [
+            "fixtures/user.json",
+            "fixtures/profile.json",
+            "fixtures/profileimage.json",
+            "fixtures/post.json",
+            "fixtures/postimage.json",
+            "fixtures/like.json",
+            "fixtures/comment.json",
+            "fixtures/follow.json",
+        ]
+        for fixture_path in fixture_paths:
+            call_command("loaddata", fixture_path)
+
+        self.stdout.write(self.style.SUCCESS("Database fixtures loaded successfully!"))
---
+++ api/apps/core_app/management/commands/create_fixtures.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from django.core import serializers
+from django.apps import apps
+
+
+class Command(BaseCommand):
+    help = "Create a fixture file for each model in the project."
+
+    def handle(self, *args, **options):
+        # Create fixtures
+        models = [
+            "user",
+            "profile",
+            "profileimage",
+            "post",
+            "postimage",
+            "like",
+            "comment",
+            "follow",
+        ]
+        for model in apps.get_models():
+            model_name = model.__name__.lower()
+            if model_name in models:
+                fixture_file = f"fixtures/{model_name}.json"
+                with open(fixture_file, "w") as f:
+                    data = serializers.serialize("json", model.objects.all())
+                    f.write(data)
+                self.stdout.write(
+                    self.style.SUCCESS(f"Created fixture file: {fixture_file}")
+                )
