02b0f49d752ea685df31d90d772e05a8e5c65a56
Brandon Dykun <brandon.dykun@gmail.com>
2024-12-26 14:56:49 -0500

    fix: add likes and comments to read only fields list
    Added likes and comments to read_only_fields list in PostSerializer.

--- api/apps/posts_app/serializers.py
+++ api/apps/posts_app/serializers.py
@@ -189,7 +189,7 @@ class PostSerializer(serializers.ModelSerializer):
             "likes",
             "comments",
         ]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at", "likes", "comments"]
 
 
 class PostDetailedSerializer(serializers.ModelSerializer):
