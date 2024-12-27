ef4a9b5b7dc22d9f218dc55c5b35b5445405478a
Brandon Dykun <brandon.dykun@gmail.com>
2024-12-26 19:04:00 -0500

    test: update posts api tests to use PostsAppTestHelper
    Updated posts api tests to use helper class.

--- api/apps/posts_app/tests/test_posts_api.py
+++ api/apps/posts_app/tests/test_posts_api.py
@@ -2,44 +2,18 @@
 Tests for the posts api.
 """
 
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-
-from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
+from apps.core_app.models import Post
 
-from apps.core_app.models import Profile, Post
-
-CREATE_POST_URL = reverse("posts_app:create_post")
-
-
-def create_user(**params):
-    """Create and return new User."""
-    return get_user_model().objects.create_user(**params)
+from .util import CREATE_POST_URL, PostsAppTestHelper
 
 
-def create_profile(**params):
-    """Create and return new Profile."""
-    return Profile.objects.create(**params)
-
-
-class PrivatePostsApiTests(APITestCase):
+class PrivatePostsApiTests(PostsAppTestHelper):
     """Test the private features of the Posts API."""
 
     def setUp(self):
-        user_details = {
-            "email": "test@example.com",
-            "password": "test-user-password-123",
-        }
-        self.user = create_user(**user_details)
-        profile_details = {
-            "username": "test_username",
-            "about": "Test about text.",
-            "user": self.user,
-        }
-        self.profile = create_profile(**profile_details)
-        self.client = APIClient()
+        super(self.__class__, self).setUp()
+        # extend setUp by authenticating self.profile
         self.client.force_authenticate(user=self.user)
         self.client.credentials(HTTP_AUTH_PROFILE_ID=self.profile.id)
 
@@ -48,6 +22,8 @@ class PrivatePostsApiTests(APITestCase):
         Test creating a Post without a caption returns 400 error and
         does not create a Post object in the database.
         """
+        starting_post_count = len(Post.objects.all())
+
         new_post = {
             "caption": "",
             "profileId": self.profile.id,
@@ -58,10 +34,14 @@ class PrivatePostsApiTests(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
         all_posts = Post.objects.all()
-        self.assertEqual(len(all_posts), 0)
+        self.assertEqual(len(all_posts), starting_post_count)
 
     def test_create_post_success(self):
-        """Test successfully creating a Post"""
+        """
+        Test successfully creating a Post returns 201 and
+        creates Post object in database.
+        """
+        starting_post_count = len(Post.objects.all())
 
         new_post = {
             "caption": "Test caption",
@@ -81,8 +61,7 @@ class PrivatePostsApiTests(APITestCase):
             "pet_type": None,
         }
         self.assertEqual(res.data["profile"], expected_profile)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
         all_posts = Post.objects.all()
-
-        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(len(all_posts), 1)
+        self.assertEqual(len(all_posts), starting_post_count + 1)
