c36aa26059db20bd42bd1cdde0be6d92d2f732e2
Brandon Dykun <brandon.dykun@gmail.com>
2025-01-12 22:29:02 -0500

    feat: add post id to Post str method
    Added Post id to str method to help identify the post in admin view.

--- api/apps/core_app/models.py
+++ api/apps/core_app/models.py
@@ -113,7 +113,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.caption
+        return f"Post {self.id} - {self.caption}"
 
 
 def post_image_path(instance, filename):
